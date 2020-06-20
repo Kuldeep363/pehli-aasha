c54fa9ebb8c084357db7e4cbf3659fd8a20b86fd
KuldeepRawat363 <Kuldeep363@users.UI.github.com>
2020-06-20 10:29:10 +0530

    added storages

--- ngo/settings.py
+++ ngo/settings.py
@@ -125,12 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR,'pehliaasha/static')
-# ]
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # s3 config
 AWS_ACCESS_KEY_ID = 'AKIAVCY3CKR47XQZZG54'
