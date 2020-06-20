51eaaf914e6e5d2b4bbf69ea0aef512608a901de
KuldeepRawat363 <Kuldeep363@users.UI.github.com>
2020-06-20 10:00:47 +0530

    added storages

--- ngo/settings.py
+++ ngo/settings.py
@@ -134,6 +134,10 @@ AWS_ACCESS_KEY_ID = 'AKIAVCY3CKR47XQZZG54'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'pehliaashango'
 
+AWS_LOCATION = 'static'
+AWS_MEDIA_LOCATION = 'media'
+STATIC_URL = 'https://pehliaashango.s3-us-west-1.amazonaws.com/static'
+
 AWS_S3_FILE_OVERWRITE = False 
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
